648982e2a4af164080501ce6ef14fe8fde8746e8
Shehroz Ali <sshehroza13@gmail.com>
2022-10-01 13:58:01 +0500

    StudentController: Corrected bug in annotations

--- src/main/java/com/my/app/student/StudentController.java
+++ src/main/java/com/my/app/student/StudentController.java
@@ -2,6 +2,7 @@ package com.my.app.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Array;
@@ -9,13 +10,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("api/v1")
 public class StudentController {
     private static final List<Student> students = Arrays.asList(
             new Student("1", "Shehroz"),
             new Student("2", "Saad"),
             new Student("3", "Ali")
     );
+
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
         return students.stream()
