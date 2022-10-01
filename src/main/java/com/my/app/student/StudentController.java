93dc92d9127dafdfe5013155540a568b1d0bb635
Shehroz Ali <sshehroza13@gmail.com>
2022-10-01 13:34:10 +0500

    StudentController: Added @RestController annotation StudentController: Created getStudent() method with @PathVariable("studentId")

--- src/main/java/com/my/app/student/StudentController.java
+++ src/main/java/com/my/app/student/StudentController.java
@@ -1,4 +1,13 @@
 package com.my.app.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1")
 public class StudentController {
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+
+    }
 }
