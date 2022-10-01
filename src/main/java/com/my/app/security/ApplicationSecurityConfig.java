1673d67ccbbac733e670e23ba8ddb296bd2ff41e
Shehroz Ali <sshehroza13@gmail.com>
2022-10-01 14:23:38 +0500

    ApplicationSecurityConfig: Added @EnableWebSecurity, @Configuration

--- src/main/java/com/my/app/security/ApplicationSecurityConfig.java
+++ src/main/java/com/my/app/security/ApplicationSecurityConfig.java
@@ -1,4 +1,9 @@
 package com.my.app.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class ApplicationSecurityConfig {
 }
