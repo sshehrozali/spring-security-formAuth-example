fde567f534f3eeb90499b2f710ad93c424188123
Shehroz Ali <sshehroza13@gmail.com>
2022-10-01 13:38:31 +0500

    Student: Added @AllArgsConstructor, @NoArgsConstructor, @Getter

--- src/main/java/com/my/app/student/Student.java
+++ src/main/java/com/my/app/student/Student.java
@@ -1,4 +1,13 @@
 package com.my.app.student;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Student {
+    private String studentId;
+    private String studentName;
 }
