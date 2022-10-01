96e5cf16e7cf3494a8a0d3647b99ca0095a56581
Shehroz Ali <sshehroza13@gmail.com>
2022-10-01 13:47:20 +0500

    StudentController: Implemented List<Students> containing some students StudentController: Returned Student matching studentId using stream().filter()

--- src/main/java/com/my/app/student/StudentController.java
+++ src/main/java/com/my/app/student/StudentController.java
@@ -4,10 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController("/api/v1")
 public class StudentController {
+    private static final List<Student> students = Arrays.asList(
+            new Student("1", "Shehroz"),
+            new Student("2", "Saad"),
+            new Student("3", "Ali")
+    );
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable("studentId") Integer studentId) {
-
+        return students.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student not found by ID: " + studentId));
     }
 }
