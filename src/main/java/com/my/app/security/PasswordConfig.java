1e12ce6e3772d53208f81968316ff8e16899e1e6
Shehroz Ali <sshehroza13@gmail.com>
2022-10-03 13:57:23 +0500

    PasswordConfig: Added @Configuration annotation PasswordConfig: Created passwordEncoder() method

--- src/main/java/com/my/app/security/PasswordConfig.java
+++ src/main/java/com/my/app/security/PasswordConfig.java
@@ -1,2 +1,11 @@
-package com.my.app.security;public class PasswordConfig {
+package com.my.app.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    public PasswordEncoder passwordEncoder() {
+
+    }
 }
