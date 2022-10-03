49bd5ad4d25d8207971057a50fb375d8a40af4d3
Shehroz Ali <sshehroza13@gmail.com>
2022-10-03 14:41:48 +0500

    ApplicationUserPermissions: Defined STUDENT_READ, STUDENT_WRITE, COURSES_READ, COURSES_WRITE ApplicationUserPermissions: Added constructor ApplicaionUserPermissions: Defined getter that returns permissions

--- src/main/java/com/my/app/security/ApplicationUserPermissions.java
+++ src/main/java/com/my/app/security/ApplicationUserPermissions.java
@@ -1,4 +1,19 @@
 package com.my.app.security;
 
 public enum ApplicationUserPermissions {
+    STUDENT_READ("student:read"),
+    STUDENT_WRITE("student:write"),
+
+    COURSES_READ("courses:read"),
+    COURSES_WRITE("courses:write");
+
+    private final String permissions;
+
+    ApplicationUserPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
 }
