512b00a79257ab7cfc0df93d514c8dc4703cbecc
Shehroz Ali <sshehroza13@gmail.com>
2022-10-04 13:22:24 +0500

    StudentManagementController: Copy pasted List<Student>

--- src/main/java/com/my/app/student/StudentManagementController.java
+++ src/main/java/com/my/app/student/StudentManagementController.java
@@ -3,7 +3,15 @@ package com.my.app.student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/management/api/v1/students")
 public class StudentManagementController {
+    private static final List<Student> students = Arrays.asList(
+            new Student(1, "Shehroz"),
+            new Student(2, "Saad"),
+            new Student(3, "Ali")
+    );
 }
