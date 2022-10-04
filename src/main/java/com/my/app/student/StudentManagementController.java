dc4be477e125775bdfaf92b38e231f7070196137
Shehroz Ali <sshehroza13@gmail.com>
2022-10-04 13:21:20 +0500

    StudentManagementController: Annotated the class with @RestController and @RequestMapping

--- src/main/java/com/my/app/student/StudentManagementController.java
+++ src/main/java/com/my/app/student/StudentManagementController.java
@@ -1,2 +1,9 @@
-package com.my.app.student;public class StudentManagementController {
+package com.my.app.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management/api/v1/students")
+public class StudentManagementController {
 }
