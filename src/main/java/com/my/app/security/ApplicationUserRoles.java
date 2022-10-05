3a8e3cbb9fb907c8379b5d150facf0b35fff6441
Shehroz Ali <sshehroza13@gmail.com>
2022-10-05 13:16:57 +0500

    ApplicationUserRoles: Implemented getGrantedAuthorities() method returning Set<SimpleGrantedAuthority>

--- src/main/java/com/my/app/security/ApplicationUserRoles.java
+++ src/main/java/com/my/app/security/ApplicationUserRoles.java
@@ -1,8 +1,11 @@
 package com.my.app.security;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public enum ApplicationUserRoles {
     STUDENT(Sets.newHashSet()),
@@ -24,6 +27,14 @@ public enum ApplicationUserRoles {
     }
 
     public Set<ApplicationUserPermissions> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermissions()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
         return permissions;
     }
 }
